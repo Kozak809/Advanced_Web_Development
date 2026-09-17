ea4371b653aebf20a9f6c0872722e5b3f737a33a
Kozak809 <kozak800009@gmail.com>
2026-09-17 07:34:24 +0300

    feat: add main.md and index.php for lab 2

--- lab2/index.php
+++ lab2/index.php
@@ -4,13 +4,11 @@ echo "Привет, мир!";
 echo "Hello, World with echo!";
 print "Hello, World with print!";
 
-$days = 228;
+$days = (int) 228;
 $message = "Все возвращаются на работу!";
-// Через конкотенацию
 
 echo "Через " . $days . " дней " . $message . "<br />";
-// С помощью двойных кавычек
+
 echo "Через $days дней $message" . "<br />";
 
-// С помощью фигурных скобок
 echo "Через {$days} дней {$message}<br />";
